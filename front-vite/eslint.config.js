3643d036e157693a3081c86a7d1498d16c451c04
jhbbong <jhbong@danalenter.co.kr>
2024-12-30 13:38:51 +0900

    feat:프로젝트셋팅

---
+++ front-vite/eslint.config.js
@@ -0,0 +1,34 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+          ecmaVersion: 'latest',  // 최신 ECMAScript 문법 지원
+          ecmaFeatures: {jsx: true},  // JSX 문법을 허용 (React에서 사용)
+          sourceType: 'module',  // ECMAScript 모듈(ESM) 사용
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...js.configs.recommended.rules,          // EsLint 기본 권장 규칙 적용
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+)
