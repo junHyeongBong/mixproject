a9a927184232e05dd746a006be65c6ec62e05295
dante <heywheredante@gmail.com>
2025-01-07 01:05:11 +0900

    feat: 환경설정 셋팅 (주석 설명)

--- server/src/main/java/com/untitled/server/ServerApplication.java
+++ server/src/main/java/com/untitled/server/ServerApplication.java
@@ -4,7 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableJpaAuditing  //엔티티의 생성 및 수정 시간을 자동으로 기록
+@EnableWebMvc
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class})
 public class ServerApplication {
 
